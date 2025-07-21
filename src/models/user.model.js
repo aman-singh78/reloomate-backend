2f8d7ae39ed428d96969947bac15d1f51cb29ff7
Aman Thapa <amansinghthapa33@gmail.com>
2025-07-21 20:45:42 +0530

    Add pre-save hook to capitalize name before saving user

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -5,21 +5,36 @@ const userSchema = new mongoose.Schema(
     name: {
       type: String,
       required: true,
+      minLength: 2,
+      maxLength: 50,
+      trim: true,
     },
     email: {
       type: String,
       required: true,
       unique: true,
+      lowercase: true,
     },
     password: {
       type: String,
       required: true,
+      minLength: 6,
     },
   },
   {
     timestamps: true,
   }
 );
+
+userSchema.pre("save", async function (next) {
+  this.name = this.name
+    .split(" ")
+    .map(
+      (word) => word.charAt(0).toUpperCase + word.slice(1).toLocaleUpperCase()
+    )
+    .join(" ");
+  next();
+});
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
