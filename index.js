589f724b4976a0c0de35ec563d7f8c4e07a25b38
Aman Thapa <amansinghthapa33@gmail.com>
2025-07-22 12:39:49 +0530

    index.js updated

--- index.js
+++ index.js
@@ -11,7 +11,12 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
